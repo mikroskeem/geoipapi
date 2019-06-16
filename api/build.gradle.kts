33792834edad9d2699dd177cbc03c4289d897516
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-06-16 23:06:38 +0300

    Add publishing configuration

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,7 +1,35 @@
 plugins {
     `java-library`
+    `maven-publish`
 }
 
 dependencies {
     api("org.checkerframework:checker-qual:${rootProject.extra["checkerQualVersion"]}")
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allJava)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "eu.mikroskeem.geoipapi"
+            artifactId = "api"
+
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+        }
+    }
+
+    repositories {
+        mavenLocal()
+    }
 }
